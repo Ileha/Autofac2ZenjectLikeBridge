87ebc91391d5f22c730b138ee8a24499bfe7f318
alexey <i.leha98@mail.ru>
2025-09-04 10:20:33 +0400

    +final edition

--- Autofac2ZenjectLikeBridge/DIExtensionsSubScopeFactories.cs
+++ Autofac2ZenjectLikeBridge/DIExtensionsSubScopeFactories.cs
@@ -404,6 +404,208 @@ namespace Autofac2ZenjectLikeBridge
 
 #endregion
 
+#region RegisterPlaceholderFactoryExtended
+
+        public static IExtendedFactoryBuilder<
+            TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TP2,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TP2, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TP2, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TP2, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TP2,
+			TP3,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TP2, TP3, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TP2, TP3, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TP2, TP3, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TP2,
+			TP3,
+			TP4,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TP2, TP3, TP4, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TP2, TP3, TP4, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TP2, TP3, TP4, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TP2,
+			TP3,
+			TP4,
+			TP5,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TP2, TP3, TP4, TP5, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TP2, TP3, TP4, TP5, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TP2, TP3, TP4, TP5, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TP2,
+			TP3,
+			TP4,
+			TP5,
+			TP6,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TP2,
+			TP3,
+			TP4,
+			TP5,
+			TP6,
+			TP7,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TP2,
+			TP3,
+			TP4,
+			TP5,
+			TP6,
+			TP7,
+			TP8,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TP8, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TP8, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TP8, TInstance, TPlaceholderFactory>(builder);
+        }
+
+        public static IExtendedFactoryBuilder<
+            TP0,
+			TP1,
+			TP2,
+			TP3,
+			TP4,
+			TP5,
+			TP6,
+			TP7,
+			TP8,
+			TP9,
+			TInstance,
+            TPlaceholderFactory,
+            SimpleActivatorData> RegisterPlaceholderFactoryExtended<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TP8, TP9, TInstance, TPlaceholderFactory>(this ContainerBuilder builder)
+            where TInstance : class
+            where TPlaceholderFactory : PlaceholderFactory<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TP8, TP9, TInstance>
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return new ExtendedPlaceholderFactoryBuilder<TP0, TP1, TP2, TP3, TP4, TP5, TP6, TP7, TP8, TP9, TInstance, TPlaceholderFactory>(builder);
+        }
+
+#endregion
+
 #region PlaceholderFactoryFromSubScope
 
         public static ISubScopeFactoryBuilder<
