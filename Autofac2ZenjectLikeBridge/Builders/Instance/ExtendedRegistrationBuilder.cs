1c4eab0c466b33643ac46ec347ef312b0dc84260
alexey <i.leha98@mail.ru>
2025-09-06 22:22:05 +0400

    +remove unrelated to subscope functionality

--- Autofac2ZenjectLikeBridge/Builders/Instance/ExtendedRegistrationBuilder.cs
+++ Autofac2ZenjectLikeBridge/Builders/Instance/ExtendedRegistrationBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Autofac;
-using Autofac.Builder;
 using Autofac2ZenjectLikeBridge.Interfaces.Builders.Instance;
 
 namespace Autofac2ZenjectLikeBridge.Builders.Instance
@@ -13,24 +12,6 @@ namespace Autofac2ZenjectLikeBridge.Builders.Instance
         public ExtendedRegistrationBuilder(ContainerBuilder builder)
         {
             Builder = builder ?? throw new ArgumentNullException(nameof(builder));
-        }
-
-        // public ISubScopeBuilder<TComponentDisposable> FromSubScope<TComponentDisposable>()
-        //     where TComponentDisposable : TComponent, IDisposable
-        // {
-        //     return new SubScopeBuilder<TComponentDisposable>(_builder);
-        // }
-
-        public IRegistrationBuilder<TComponent, SimpleActivatorData, SingleRegistrationStyle> FromInstance(TComponent instance)
-        {
-            return Builder
-                .Register(_ => instance);
-        }
-
-        public IRegistrationBuilder<TComponent, SimpleActivatorData, SingleRegistrationStyle> FromNewInstance()
-        {
-            return Builder
-                .Register(context => context.CreateInstance<TComponent>());
         }
     }
 }
--- Autofac2ZenjectLikeBridge/Interfaces/Builders/Instance/IExtendedRegistrationBuilder.cs
+++ Autofac2ZenjectLikeBridge/Interfaces/Builders/Instance/IExtendedRegistrationBuilder.cs
@@ -1,18 +1,7 @@
-﻿using Autofac.Builder;
-
-namespace Autofac2ZenjectLikeBridge.Interfaces.Builders.Instance
+﻿namespace Autofac2ZenjectLikeBridge.Interfaces.Builders.Instance
 {
     public interface IExtendedRegistrationBuilder<TComponent> : IExtendedBuilderBase
         where TComponent : class
     {
-        IRegistrationBuilder<
-            TComponent,
-            SimpleActivatorData,
-            SingleRegistrationStyle> FromInstance(TComponent instance);
-
-        IRegistrationBuilder<
-            TComponent,
-            SimpleActivatorData,
-            SingleRegistrationStyle> FromNewInstance();
     }
 }
